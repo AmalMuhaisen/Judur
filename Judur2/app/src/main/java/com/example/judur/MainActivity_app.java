512f770bbe49434cb1cab7db03ceb916efea0887
Amal Muhaisen <am.failaka2003@gmail.com>
2020-10-20 15:23:07 +0300

    done

--- Judur2/app/src/main/java/com/example/judur/MainActivity_app.java
+++ Judur2/app/src/main/java/com/example/judur/MainActivity_app.java
@@ -1,6 +1,7 @@
 package com.example.judur;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -14,12 +15,20 @@ public class MainActivity_app extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_app);
 
-        ArrayList<MediaStore.Images> Appetizars = new ArrayList<>();
+        ArrayList<food> Appetizars = new ArrayList<>();
 
         food fo1 = new food("سلطة كينوا",R.drawable.ke);
         food fo2 = new food("سلطة فاصوليا بالذرة",R.drawable.fa);
         food fo3 = new food("شوربة عدس",R.drawable.ad);
         food fo4 = new food("حمص",R.drawable.hm);
-        food fo5 = new food("",R.drawable.ga);
+        food fo5 = new food("شوربة يقطين",R.drawable.ga);
+
+        Appetizars.add(fo1);
+        Appetizars.add(fo2);
+        Appetizars.add(fo3);
+        Appetizars.add(fo4);
+        Appetizars.add(fo5);
+
+        RecyclerView rv =findViewById(R.id.rv);
     }
 }
